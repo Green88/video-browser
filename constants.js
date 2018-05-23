f716946893eab3b1e72b0497ef354394a9b4e7ea
Green88 <taniagr0808@gmail.com>
2018-05-23 16:35:28 +0300

    yt videos exercise

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const YOUTUBE_API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+export const YOUTUBE_ENDPOINT = 'https://www.googleapis.com/youtube/v3/search';
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+        <SearchBar @termChange="onTermChange" />
+        <div class="row">
+            <VideoDetail :video="video"></VideoDetail>
+            <VideoList :videos="videos"  @videoSelect="onVideoSelect"></VideoList>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
+    import SearchBar from './components/SearchBar';
+    import VideoList from './components/VideoList';
+    import VideoDetail from './components/VideoDetail';
+    import { YOUTUBE_API_KEY } from '../constants';
+    import { YOUTUBE_ENDPOINT } from '../constants';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: "App",
+        components: {
+            VideoDetail,
+            VideoList,
+            SearchBar
+        },
+        data: function() {
+            return {
+                videos: [],
+                video: null
+            };
+        },
+        methods: {
+            onTermChange: function(searchTerm) {
+                axios.get(YOUTUBE_ENDPOINT, {
+                    params: {
+                        key: YOUTUBE_API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => {
+                    this.videos = response.data.items;
+                });
+            },
+            onVideoSelect: function(video) {
+                // eslint-disable-next-line
+                console.log(video);
+                this.video = video;
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
